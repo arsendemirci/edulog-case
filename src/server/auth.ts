443090acd2e0b15cc3e74cfc6e1596008e210be9
arsen.demirci <arsen.demirci@eczacibasi.com.tr>
2024-08-31 01:46:32 +0300

    added redirect option

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -27,7 +27,7 @@ export const authOptions: NextAuthOptions = {
       if (url.startsWith("/")) return `${baseUrl}${url}`;
       // // Allows callback URLs on the same origin
       else if (new URL(url).origin === baseUrl) return url;
-      return url;
+      return baseUrl;
     },
   },
   pages: {
