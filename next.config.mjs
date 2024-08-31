deec004248435468028e308aeffda0763283deca
arsen.demirci <arsen.demirci@eczacibasi.com.tr>
2024-08-31 21:46:39 +0300

    finally figured out configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       destination: "/login",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
   sassOptions: {
     prependData: `@import "/src/style/_variables.scss";
        @import "/src/style/_mixins.scss";`,
   },
+  // env: {
+  //   NEXTAUTH_SECRET: "B0si49v3XOb92VY8RUfPsbeLiB6BdkdNIkvRjdqJyVg=",
+  //   NEXTAUTH_URL: "http://localhost:3001/api/auth",
+  // },
 };
 
 export default nextConfig;
