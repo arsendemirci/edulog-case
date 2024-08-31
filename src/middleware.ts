f5f5be2c4c5db2b1b1e0e045ed2727a6ee31491a
arsen.demirci <arsen.demirci@eczacibasi.com.tr>
2024-08-31 22:27:57 +0300

    fixed session cookie issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,22 @@
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-export function middleware(request: NextRequest) {
+import { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
   //protect the pages except login, from unauthenticated users
-  //if user has no session token means user is not logged in so redirect them to login page
-  const session = request.cookies.get("next-auth.session-token");
+  //if user has no session cookie means user is not logged in so redirect them to login page
+  const cookies: RequestCookie[] = request.cookies.getAll();
+
+  const sessionCookie = cookies.find((c) =>
+    c.name.includes("next-auth.session-token")
+  );
 
   if (request.nextUrl.pathname == "/login") {
-    if (session && session.value) {
+    if (sessionCookie && sessionCookie.value) {
       return NextResponse.redirect(new URL("/home", request.url));
     }
   } else {
-    if (!session || !session.value) {
+    if (!sessionCookie || !sessionCookie.value) {
       return NextResponse.redirect(new URL("/login", request.url));
     }
   }
